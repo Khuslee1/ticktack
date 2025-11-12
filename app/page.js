957da0c365a37b15dfb367417679035bb082587a
Khuslee1 <Khuselee11@gmail.com>
2025-11-12 12:43:31 +0800

    nov12day

--- app/page.js
+++ app/page.js
@@ -1,65 +1,109 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import { use, useState } from "react";
 
 export default function Home() {
+  const [arr, setArr] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [turn, setTurn] = useState(1);
+  const [win, setWin] = useState(0);
+  const conditionWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  console.log("Win", win);
+
+  const ashiglagch = () => {
+    if (win == 1) {
+      console.log("blue win");
+    } else if (win == 2) {
+      console.log("red win");
+    }
+  };
+
+  const checker = () => {
+    for (let i = 0; i < conditionWin.length; i++) {
+      const [a, b, c] = conditionWin[i];
+      if (arr[a] == 1 && arr[b] == 1 && arr[c] == 1) {
+        setWin(1);
+        return;
+      }
+      if (arr[a] == 2 && arr[b] == 2 && arr[c] == 2) {
+        setWin(2);
+        return;
+      }
+    }
+  };
+
+  const budagch = (llr) => {
+    if (arr[llr] !== 0 || win !== 0) return;
+    if (turn === 1) {
+      setTurn(2);
+      const newArr = arr.map((el, i) => (llr === i ? 1 : el));
+      setArr(newArr);
+    } else {
+      setTurn(1);
+      const newArr = arr.map((el, i) => (llr === i ? 2 : el));
+      setArr(newArr);
+    }
+  };
+  // const clickTotal = (llr) => {
+
+  //   if (turn === 1) {
+  //     setTurn(2);
+  //     const newArr = arr.map((el, i) => (llr === i ? 1 : el));
+  //     setArr(newArr);
+  //   } else {
+  //     setTurn(1);
+  //     const newArr = arr.map((el, i) => (llr === i ? 2 : el));
+  //     setArr(newArr);
+  //   }
+
+  //   for (let i = 0; i < conditionWin.length; i++) {
+  //     const [a, b, c] = conditionWin[i];
+  //     if (arr[a] == 1 && arr[b] == 1 && arr[c] == 1) {
+  //       setWin(1);
+  //     }
+  //     if (arr[a] == 2 && arr[b] == 2 && arr[c] == 2) {
+  //       setWin(2);
+  //     }
+  //   }
+
+  //   if (win == 1) {
+  //     console.log("blue win");
+  //     return;
+  //   } else if (win == 2) {
+  //     console.log("red win");
+  //     return;
+  //   }
+  // };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="h-screen w-screen flex justify-center items-center flex-col gap-3">
+      <div className="h-[150px] w-[150px] border border-black flex flex-wrap justify-center items-center">
+        {arr.map((ele, i) => {
+          return (
+            <div
+              key={i}
+              className={`h-[49px] w-[49px] border border-black ${
+                ele == 1 ? "bg-blue-500" : ele == 2 ? "bg-red-500" : "bg-white"
+              }`}
+              onClick={() => {
+                ashiglagch();
+                budagch(i);
+                checker();
+              }}
+            ></div>
+          );
+        })}
+      </div>
+      <Button variant="outline">Restart</Button>
     </div>
   );
 }
